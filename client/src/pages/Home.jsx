6ec379a5595ff574f1425fab47e1a5419b6549fe
Waseef Mohammad Khan <waseef.khan@kcl.ac.uk>
2024-03-09 17:56:44 +0000

    updated api

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import {
   Container,
   Row,
@@ -7,90 +7,128 @@ import {
   Card,
   Button,
   Nav,
+  Form,
 } from "react-bootstrap";
 
 import dogHappy from "../assets/images/dog_happy.jpg";
 
+import { getTasks } from "../api/tasks";
+
 export default function Home() {
+  const [tasks, setTasks] = useState([]);
+  //loading
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getTasks()
+      .then((response) => response.data)
+      .then((data) => setTasks(data.tasks))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
   // return a grid with one row and two columns, the first column should be 3/12 and the second column should be 9/12
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
   return (
     <>
-      <Row style={{ height: "100vh" }}>
+      <Row style={{ height: "100vh" }} s>
         <Col
           xs={3}
           style={{
             backgroundColor: "beige",
+            justifyContent: "flex-start", // change from "center" to "flex-start"
             display: "flex",
-            justifyContent: "center",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Nav
-            variant="tabs"
-            defaultActiveKey="mon"
-            style={{ marginTop: "1rem", fontWeight: "bold" }}
-          >
-            <Nav.Item>
-              <Nav.Link
-                eventKey="mon"
-                style={{ backgroundColor: "grey", color: "white" }}
-              >
-                Mon
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link
-                eventKey="tue"
-                style={{ backgroundColor: "grey", color: "white" }}
-              >
-                Tue
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link
-                eventKey="wed"
-                style={{ backgroundColor: "grey", color: "white" }}
-              >
-                Wed
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link
-                eventKey="thu"
-                style={{ backgroundColor: "grey", color: "white" }}
-              >
-                Thu
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link
-                eventKey="fri"
-                style={{ backgroundColor: "grey", color: "white" }}
-              >
-                Fri
-              </Nav.Link>
-            </Nav.Item>
-          </Nav>
+          <div style={{ marginTop: "1rem", marginBottom: "1rem" }}>
+            <Nav
+              variant="tabs"
+              defaultActiveKey="mon"
+              style={{ fontWeight: "bold" }}
+            >
+              <Nav.Item>
+                <Nav.Link
+                  eventKey="mon"
+                  style={{ backgroundColor: "grey", color: "white" }}
+                >
+                  Mon
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link
+                  eventKey="tue"
+                  style={{ backgroundColor: "grey", color: "white" }}
+                >
+                  Tue
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link
+                  eventKey="wed"
+                  style={{ backgroundColor: "grey", color: "white" }}
+                >
+                  Wed
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link
+                  eventKey="thu"
+                  style={{ backgroundColor: "grey", color: "white" }}
+                >
+                  Thu
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link
+                  eventKey="fri"
+                  style={{ backgroundColor: "grey", color: "white" }}
+                >
+                  Fri
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </div>
+          <div>
+            <Card style={{ width: "18rem" }}>
+              <Card.Header>Add Task</Card.Header>
+              <Card.Body>
+                {/* Add task form */}
+                <Form>
+                  <Form.Group controlId="taskName">
+                    <Form.Label>Task Name</Form.Label>
+                    <Form.Control type="text" placeholder="Enter task name" />
+                  </Form.Group>
+                  <Form.Group controlId="taskDescription">
+                    <Form.Label>Task Description</Form.Label>
+                    <Form.Control
+                      as="textarea"
+                      rows={3}
+                      placeholder="Enter task description"
+                    />
+                  </Form.Group>
+                  <Button variant="primary" type="submit">
+                    Add
+                  </Button>
+                </Form>
+                {/* Task list */}
+                <ul>
+                  {/* Render tasks dynamically */}
+                  {tasks.map((task) => (
+                    <li key={task.id}>{task.name}</li>
+                  ))}
+                </ul>
+              </Card.Body>
+            </Card>
+          </div>
         </Col>
         <Col xs={9}>
           <Container style={{ height: "100%" }}>
             <h1>Progress</h1>
             <ProgressBar now={60} />
-            <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src={dogHappy} />
-              <Card.Body>
-                <Card.Title>Happy Dog</Card.Title>
-                <Card.Text>
-                  This is a happy dog. He is happy because he has completed all
-                  his tasks and habits for today.
-                </Card.Text>
-                <Button
-                  variant="primary"
-                  style={{ backgroundColor: "grey", color: "white" }}
-                >
-                  Edit
-                </Button>
-              </Card.Body>
-            </Card>
           </Container>
         </Col>
       </Row>
